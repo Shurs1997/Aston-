bcef3d104c549fa30fb6b430ac856771c2906819
Selivanov Aleksandr <hai82870@gmail.com>
2025-04-05 13:32:43 +0300

    add  Lesson1 on git

---
+++ src/main/java/Lesson1/Lesson5.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+        compareNumbers(16, 5);
+
+    }
+
+    public static void compareNumbers(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
