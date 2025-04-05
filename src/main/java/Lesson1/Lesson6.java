780d1ff4340444c5115e108d6e67e54b75378db8
Selivanov Aleksandr <hai82870@gmail.com>
2025-04-05 13:37:50 +0300

    add  Lesson1 on git

---
+++ src/main/java/Lesson1/Lesson6.java
@@ -0,0 +1,16 @@
+package Lesson1;
+
+public class Lesson6 {
+    public static void main(String[] args) {
+        printNumber(0);
+
+    }
+
+    public static void printNumber(int number) {
+        if (number >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+}
