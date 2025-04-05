5c2b69d3f2dcabed5cb572cb519f1e6fcd3b4311
Selivanov Aleksandr <hai82870@gmail.com>
2025-04-05 13:59:19 +0300

    add  Lesson1 on git

---
+++ src/main/java/Lesson1/Lesson9.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+public class Lesson9 {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2001));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
