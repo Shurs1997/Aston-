831c684bd3b275d6d41ff10959497fdc43c37fc3
Shurs1997 <git@github.com:Shurs1997/Aston-.git>
2025-04-05 11:55:31 +0300

    add Lesson1 on git

---
+++ src/main/java/Lesson1/Lesson1.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
