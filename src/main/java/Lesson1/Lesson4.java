ee8d0cb0e221a4573167c58e2647198cf4b24463
Selivanov Aleksandr <hai82870@gmail.com>
2025-04-05 13:16:19 +0300

    add  Lesson1 on git

---
+++ src/main/java/Lesson1/Lesson4.java
@@ -0,0 +1,18 @@
+package Lesson1;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 4;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
