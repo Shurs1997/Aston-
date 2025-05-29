409da0c7d9770647ad9ec760989b55e0ff11cdf7
Selivanov Aleksandr <hai82870@gmail.com>
2025-05-29 10:23:53 +0300

    Delete src/test/java/TriangleArea directory

--- src/test/java/TriangleArea/TriangleArea.java
+++
@@ -1,8 +0,0 @@
-package TriangleArea;
-
-public class TriangleArea {
-    public double calculate(double base, double height) {
-        if (base < 0 || height < 0) throw new IllegalArgumentException("Negative value");
-        return 0.5 * base * height;
-    }
-}
